61c2c262161b7c8da3fbe247a00d90c320d6b328
SaintKappa <96151089+Saint-Kappa@users.noreply.github.com>
2022-04-21 23:59:54 +0200

    no stats

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 import './style.css'
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
-import * as Stats from 'stats.js'
 
 THREE.DefaultLoadingManager.onStart = function(url, itemsLoaded, itemsTotal) {
     console.log('Started loading file.');
@@ -25,11 +24,6 @@ THREE.DefaultLoadingManager.onError = function(url) {
 
 
 window.addEventListener("resize", onWindowResize, false);
-
-var stats = new Stats();
-stats.showPanel(0); // 0: fps, 1: ms, 2: mb, 3+: custom
-stats.dom.classList.add('stats');
-document.body.appendChild(stats.dom);
 
 
 // scene, camera
